a30b5acf741976e18833a703a7842e55fea6f821
Juan Gomez <gomezsandovalpablo@gmail.com>
2025-09-09 09:30:48 -0700

    project setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Button } from "./components/ui/button";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <h1>Hola Mundo</h1>
+    <Button>Hola</Button>
+  </StrictMode>
+);
