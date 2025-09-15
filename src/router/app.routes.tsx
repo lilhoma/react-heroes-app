b0c71b32fcc0d295dee7e10173edcfcf46f0eec7
Juan Gomez <gomezsandovalpablo@gmail.com>
2025-09-14 18:32:47 -0700

    main routing

--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -1,24 +1,41 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
 import HomePage from "../heroes/pages/home/HomePage";
 import HeroPage from "@/heroes/pages/hero/HeroPage";
-import SearchPage from "@/heroes/pages/search/SearchPage";
 import AdminPage from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
+// import SearchPage from "@/heroes/pages/search/SearchPage";
+
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/heroes/1",
-    element: <HeroPage />,
-  },
-  {
-    path: "/search",
-    element: <SearchPage />,
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "heroes/1",
+        element: <HeroPage />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+    ],
   },
   {
     path: "/admin",
-    element: <AdminPage />,
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />,
+      },
+    ],
   },
 ]);
